8295098928b5006b3dd8ba78193be7756beeaed1
Alex Li <76851963+Erecutoro@users.noreply.github.com>
2022-04-06 18:47:17 +0200

    Added list refresher

--- Sample/src/app/home/home.page.ts
+++ Sample/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,58 +9,30 @@ import { Component } from '@angular/core';
 })
 
 export class HomePage {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route : Router) {}
   obj = [];
   log = "log-in";
   user = "user";
+
+  doRefresh(event) {
+    this.http.get<any>("http://localhost:3000/employees").subscribe(data => {
+      this.obj = data;
+      console.log(this.obj);
+      event.target.complete();
+    });
+    setTimeout(() => {
+      event.target.complete();
+      console.log('Async operation has ended');
+    }, 2000);
+  }
 
   checkLog() {
     localStorage.getItem("loggedIn") == "true" ? this.log = "log-out" : this.log = "log-in";
   }
 
-  edit(i : [], index : number) {
-    
+  edit(i : any) {
+    this.route.navigate(['/register', {first_name: i.first_name, last_name: i.last_name, email: i.email, id: i.id}]);
   }
-
-  // edit(i : [], index : number) {
-  //   const prompt = this.modalCtrl.create({
-  //     title: "Edit",
-  //     inputs: [{
-  //       name: "First name",
-  //       placeholder: "First name"
-  //     },
-  //     {
-  //       name: "Last name",
-  //       placeholder: "Last name"
-  //     },
-  //     {
-  //       name: "Email",
-  //       placeholder: "Email"
-  //     }],
-  //     buttons: [
-  //       {
-  //         text: "Cancel"
-  //       },
-  //       {
-  //         text: "Save",
-  //         handler: data => {
-  //           if (this.user == "admin")
-  //             console.error("Not admin");
-  //           this.obj[index].firstName = data.First;
-  //           this.obj[index].lastName = data.Last;
-  //           this.obj[index].email = data.Email;
-  //         },
-  //       },
-  //       {
-  //         text: "Delete",
-  //         handler: data => {
-  //           if (this.user == "admin")
-  //             console.error("Not admin");
-  //           this.obj.splice(index, 1);
-  //         }
-  //       }]
-  //   })
-  // }
 
   ngOnInit() {
     this.checkLog();
