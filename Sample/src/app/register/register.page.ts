50d3e3febf5818167276bcd1d711ec240f214ab0
Alex Li <76851963+Erecutoro@users.noreply.github.com>
2022-04-06 12:50:21 +0200

    Add new employee feature

--- Sample/src/app/register/register.page.ts
+++ Sample/src/app/register/register.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
   registerForm: FormGroup;
-  constructor(private router: Router, private fb: FormBuilder) { }
+  constructor(private router: Router, private fb: FormBuilder, public http: HttpClient) { }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
@@ -20,7 +22,12 @@ export class RegisterPage implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-    // this.router.navigate(['/home']);
+    if (!this.registerForm.valid) {
+      console.error("Please fill out the form correctly.");
+      return;
+    }
+    let body = this.registerForm.value;
+    this.http.post<any>("http://localhost:3000/employees", body).subscribe();
+    this.router.navigate(['/home']);
   }
 }
