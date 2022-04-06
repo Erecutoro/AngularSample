9d8b525c280bf39ad6c34970a24db1a35cdebcf2
Alex Li <76851963+Erecutoro@users.noreply.github.com>
2022-04-06 16:43:34 +0200

    Added edit boutton

--- Sample/src/app/home/home.page.ts
+++ Sample/src/app/home/home.page.ts
@@ -6,15 +6,66 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   constructor(private http: HttpClient) {}
   obj = [];
+  log = "log-in";
+  user = "user";
+
+  checkLog() {
+    localStorage.getItem("loggedIn") == "true" ? this.log = "log-out" : this.log = "log-in";
+  }
+
+  edit(i : [], index : number) {
+    
+  }
+
+  // edit(i : [], index : number) {
+  //   const prompt = this.modalCtrl.create({
+  //     title: "Edit",
+  //     inputs: [{
+  //       name: "First name",
+  //       placeholder: "First name"
+  //     },
+  //     {
+  //       name: "Last name",
+  //       placeholder: "Last name"
+  //     },
+  //     {
+  //       name: "Email",
+  //       placeholder: "Email"
+  //     }],
+  //     buttons: [
+  //       {
+  //         text: "Cancel"
+  //       },
+  //       {
+  //         text: "Save",
+  //         handler: data => {
+  //           if (this.user == "admin")
+  //             console.error("Not admin");
+  //           this.obj[index].firstName = data.First;
+  //           this.obj[index].lastName = data.Last;
+  //           this.obj[index].email = data.Email;
+  //         },
+  //       },
+  //       {
+  //         text: "Delete",
+  //         handler: data => {
+  //           if (this.user == "admin")
+  //             console.error("Not admin");
+  //           this.obj.splice(index, 1);
+  //         }
+  //       }]
+  //   })
+  // }
 
   ngOnInit() {
+    this.checkLog();
     this.http.get<any>("http://localhost:3000/employees").subscribe(data => {
       this.obj = data;
       console.log(this.obj);
     });
   }
-
 }
